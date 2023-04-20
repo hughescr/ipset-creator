abf642ba9395bbe8738e56566d010ecec3bcb42a
Craig R. Hughes <craig.git@rungie.com>
2023-04-20 14:36:38 -0400

    Ignore files generated by script; auto-refresh git

--- systemd-scripts/build-list-from-countries.sh
+++ systemd-scripts/build-list-from-countries.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+git -C country-ip-blocks pull --all -p
+
 grep -F '' country-ip-blocks/ipv4/* \
     | sed -e 's/^country-ip-blocks\/ipv4\///;s/.cidr:/\t/' \
     | tr '[:lower:]' '[:upper:]' \
