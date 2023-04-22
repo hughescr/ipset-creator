186b2fc35862b4f8963146e5cfa7c589bcb8a590
Craig R. Hughes <craig.git@rungie.com>
2023-04-22 09:41:23 -0400

    Update scripts to only run when there are changes; update submodule

--- systemd-scripts/ipset-creator.sh
+++ systemd-scripts/ipset-creator.sh
@@ -6,22 +6,28 @@ IPSET_CREATOR=$("$YARN" global bin)/ipset-creator
 COUNTRIES_DATA_GIT_DIR=/var/opt/ipset-creator
 
 cd "$COUNTRIES_DATA_GIT_DIR" || exit
-source "$SCRIPTS_DIR/build-list-from-countries.sh"
+"$SCRIPTS_DIR/build-list-from-countries.sh"
 
-NORTH_AMERICA_EXISTS=$("$IPSET" list -n -q north-america)
-if [ "$NORTH_AMERICA_EXISTS" != "north-america" ]; then
-    echo Looks like the ipsets did not previously exist, so creating them fresh
-    "$IPSET" restore -f <("$IPSET_CREATOR")
-else
-    echo Looks like the ipsets did exist, so refreshing using swaps
-    "$IPSET" restore -f <("$IPSET_CREATOR" -r)
-fi
+if [ $? -eq 1 ]; then
+    echo "Changes detected, refreshing ipsets"
+    NORTH_AMERICA_EXISTS=$("$IPSET" list -n -q north-america)
+    if [ "$NORTH_AMERICA_EXISTS" != "north-america" ]; then
+        echo Looks like the ipsets did not previously exist, so creating them fresh
+        "$IPSET" restore -f <("$IPSET_CREATOR")
+    else
+        echo Looks like the ipsets did exist, so refreshing using swaps
+        "$IPSET" restore -f <("$IPSET_CREATOR" -r)
+    fi
 
-IPV6_EXISTS=$("$IPSET" list -n -q north-america-ipv6)
-if [ "$IPV6_EXISTS" != "north-america-ipv6" ]; then
-    echo Looks like the ipv6 ipsets did not previously exist, so creating them fresh
-    "$IPSET" restore -f <("$IPSET_CREATOR" -6)
+    IPV6_EXISTS=$("$IPSET" list -n -q north-america-ipv6)
+    if [ "$IPV6_EXISTS" != "north-america-ipv6" ]; then
+        echo Looks like the ipv6 ipsets did not previously exist, so creating them fresh
+        "$IPSET" restore -f <("$IPSET_CREATOR" -6)
+    else
+        echo Looks like the ipv6 ipsets did exist, so refreshing using swaps
+        "$IPSET" restore -f <("$IPSET_CREATOR" -6 -r)
+    fi
 else
-    echo Looks like the ipv6 ipsets did exist, so refreshing using swaps
-    "$IPSET" restore -f <("$IPSET_CREATOR" -6 -r)
+    echo "No changes detected, skipping ipset refresh"
+    exit 0
 fi
